ed8ba5b2c9120359a785d09d64d790a3134c7d61
MattiaAngelini <mattiaangelini1993@gmail.com>
2025-03-25 14:11:26 +0100

    Created counter using Context API

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # React + Vite
-Esercitazione per studio di React, gestione degli stati utilizzando useState, useReducer e Redux.
+Esercitazione per studio di React, gestione degli stati utilizzando useState, useReducer, Context API e Redux.
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // import { useState, useReducer } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './redux/counterSlice.js'
-import { useReducer, useState} from 'react';
+import { useReducer, useState , useContext} from 'react';
 import './App.css'
+
+// import contesta e usa hook useContext, questi dati posso essere utilizzati in componenti a scelta,
+// incluendo il provider nel frammento vuoto <>
+import { contextStore } from './assets/store/contextStore.jsx';
 
 // REDUCER
 function counterReducer(state,action){
@@ -44,7 +48,10 @@ function App() {
   const dispatch = useDispatch()
 
   return (
-    <>
+    // di default il contesto è vuoto, quindi nel value del provider posso passare uno stato qualsiasi,
+    // (passo il counter di useState)
+    <contextStore.Provider value={{count, setCount}}>
+      
      {/* COUNTER con UseState  */}
      <h3 className='text-center'>Counter useEffect</h3>
       <div className='d-flex justify-content-center align-items-center'>
@@ -67,6 +74,19 @@ function App() {
         </div>
       </div>
 
+      {/* COUNTER con Context API 
+      Diversa gestione delle props tramite gestione dello stato, tramite 'contesto'
+      */}
+     <h3 className='text-center'>Counter ContextApi</h3>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='d-flex gap-5'>
+          
+          <button onClick={decrement2}>-</button>
+          <span>{counter2}</span>
+          <button onClick={increment2}>+</button>
+        </div>
+      </div>
+
       {/* COUNTER con Redux  */}
      <h3 className='text-center'>Counter Redux</h3>
       <div className='d-flex justify-content-center align-items-center'>
@@ -74,10 +94,9 @@ function App() {
           <button  onClick={() => dispatch(decrement())}>-</button>
           <span>{counter3}</span>
           <button  onClick={() => dispatch(increment())}>-</button>
-
         </div>
       </div>
-    </>
+    </contextStore.Provider>
   )
 }
 
